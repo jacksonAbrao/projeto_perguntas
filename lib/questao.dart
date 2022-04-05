d2b48101459f49f882a12cbf4b27fbc8dafd444a
jackson.comp1681@hotmail.com <jackson.comp1681@hotmail.com>
2022-04-05 18:09:21 -0300

    .

---
+++ lib/questao.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+
+class Questao extends StatelessWidget {
+  final String texto;
+
+  Questao(this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(texto);
+  }
+}
